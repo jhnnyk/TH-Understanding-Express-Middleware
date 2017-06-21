745f49598339e383defb7998ac37b2b2f3153b00
John Knotts <jhnnyk@gmail.com>
2017-06-21 15:11:22 -0600

    middleware basics lessons

--- 02_middleware_basics/app.js
+++ 02_middleware_basics/app.js
@@ -1,9 +1,19 @@
-const express = require('express');
-const fav = require('./fav');
-const app = express();
-app.use(fav);
+const express = require('express')
+const fav = require('./fav')
+const app = express()
+app.use(fav)
 
+app.use((req, res, next) => {
+  console.log('Hello')
+  // const err = new Error('Holy Smokes!')
+  // next(err)
+  next()
+})
 
+app.use((req, res, next) => {
+  console.log('World')
+  next()
+})
 
-app.use((req, res) => res.send('<h1>Express is working!</h1>'));
-app.listen(3000);
+app.use((req, res) => res.send('<h1>Express is working!</h1>'))
+app.listen(3000)
