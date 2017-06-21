5c27c2bdf111edc1ee751f9f029af1041243a3a3
John Knotts <jhnnyk@gmail.com>
2017-06-21 15:42:18 -0600

    multiply with configurable multiple

--- 04_middleware_in_context/app.js
+++ 04_middleware_in_context/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 
-const double = require('./double')
+const multiply = require('./multiply')
 
 const routes = require('./routes/index')
 
@@ -16,7 +16,7 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(double)
+app.use(multiply({by: 3}))
 
 app.use('/', routes)
 
